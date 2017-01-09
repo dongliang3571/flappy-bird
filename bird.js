43c34e83f091e0930b92cc0053be8f14a58ea77d
Weifan Lin <wlin15@citymail.cuny.edu>
2017-01-09 15:37:06 -0500

    jumping and falling bird

--- bird.js
+++ bird.js
@@ -1,19 +1,39 @@
-function Bird() {
-  this.death = false;
+function Bird(width, height) {
+  // initialize bird position
+  this.x = 0;
+  this.y = height / 2;
 
-  this.x = 50;
-  this.y = 50;
+  this.death = false;
+  this.velocity = 0;
+  this.gravity = -0.0001;
+  this.up = 10;
+
+  this.checkDeath = function() {
+    if (this.y <= 0) {
+      this.death = true;
+      console.log('DEAD');
+    }
+    return this.death;
+  };
+
+  this.update = function () {
+    this.velocity += this.gravity;
+    bird.y += bird.velocity;
+    console.log(this.y);
+  };
 
   this.jump = function () {
-    this.x += 10;
-    this.y += 10;
-    console.log(this.x + ' ' + this.y);
-  }
-}
+    this.velocity += this.up;
+    console.log(this.velocity);
+  };
+};
 
 
 
-// var canvas = document.getElementById("canvas");
-// var ctx = canvas.getContext("2d");
-// ctx.fillStyle = "#FF0000";
-// ctx.fillRect(0, 0, 80, 80);
+var bird = new Bird(200, 400);
+document.addEventListener("click", function(){
+  bird.jump();
+});
+while(!bird.checkDeath()) {
+  bird.update()
+};
