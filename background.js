fe8aef5216212deeb4aef5a060c3aca16833f309
Dong Liang <dongliang3571@gmail.com>
2017-01-09 15:39:22 -0500

    be able to display bg image

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
-function draw() {
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    context.fillStyle = 'red';
-    console.log(canvas.width);
-    console.log(canvas.height);
-    context.fillRect(0, 0, canvas.width, canvas.height);
+// Loading background image
+function LoadingImage() {
+    this.backgroundImage = new Image();
+    this.backgroundImage.src = "assets/bg.png";
 }
 
-draw();
+var bgImage = new LoadingImage();
+// var bgImage = new Image();
+// bgImage.src = "assets/bg.png";
+
+// Real background object
+function BackgroundObject() {
+    this.draw = function() {
+        var canvas_bg = document.getElementById('canvas-bg');
+        var context = canvas_bg.getContext('2d');
+        context.clearRect(0, 0, canvas_bg.width, canvas_bg.height);
+        context.drawImage(bgImage.backgroundImage, 0, 0, canvas_bg.width, canvas_bg.height);
+    }
+}
+
+var bgObject = new BackgroundObject();
+bgObject.draw();
