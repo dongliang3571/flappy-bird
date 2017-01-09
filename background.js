94fb3f848339b46e39a78ddaefb3cb42a32c5285
Dong Liang <dongliang3571@gmail.com>
2017-01-09 16:29:25 -0500

    finished moving background

--- background.js
+++ background.js
@@ -4,19 +4,34 @@ function LoadingImage() {
     this.backgroundImage.src = "assets/bg.png";
 }
 
-var bgImage = new LoadingImage();
-// var bgImage = new Image();
-// bgImage.src = "assets/bg.png";
 
 // Real background object
 function BackgroundObject() {
+    this.speed = 1;
+    this.x = 0;
+    this.y = 0;
     this.draw = function() {
-        var canvas_bg = document.getElementById('canvas-bg');
-        var context = canvas_bg.getContext('2d');
         context.clearRect(0, 0, canvas_bg.width, canvas_bg.height);
-        context.drawImage(bgImage.backgroundImage, 0, 0, canvas_bg.width, canvas_bg.height);
+        context.drawImage(bgImage.backgroundImage, this.x, this.y, canvas_bg.width, canvas_bg.height);
+        context.drawImage(bgImage.backgroundImage, -canvas_bg.width+this.x, this.y, canvas_bg.width, canvas_bg.height);
+        this.x += this.speed;
+        if (this.x >= canvas_bg.width) this.x = 0;
     }
 }
 
+// get canvas and set width and height
+var canvas_bg = document.getElementById('canvas-bg');
+canvas_bg.width = 910;
+canvas_bg.height = 422;
+
+var context = canvas_bg.getContext('2d');
+var bgImage = new LoadingImage();
 var bgObject = new BackgroundObject();
-bgObject.draw();
+
+
+// start the loop
+function start() {
+    bgObject.draw();
+}
+
+setInterval(start, 10);
