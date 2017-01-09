17f0cbbd67d0408bbd90666dff2e9b8bb2d9e877
Dong Liang <dongliang3571@gmail.com>
2017-01-09 16:33:52 -0500

    make image as parameter of background obj

--- background.js
+++ background.js
@@ -6,14 +6,15 @@ function LoadingImage() {
 
 
 // Real background object
-function BackgroundObject() {
+function BackgroundObject(image) {
+    this.image = image;
     this.speed = 1;
     this.x = 0;
     this.y = 0;
     this.draw = function() {
         context.clearRect(0, 0, canvas_bg.width, canvas_bg.height);
-        context.drawImage(bgImage.backgroundImage, this.x, this.y, canvas_bg.width, canvas_bg.height);
-        context.drawImage(bgImage.backgroundImage, -canvas_bg.width+this.x, this.y, canvas_bg.width, canvas_bg.height);
+        context.drawImage(this.image.backgroundImage, this.x, this.y, canvas_bg.width, canvas_bg.height);
+        context.drawImage(this.image.backgroundImage, -canvas_bg.width+this.x, this.y, canvas_bg.width, canvas_bg.height);
         this.x += this.speed;
         if (this.x >= canvas_bg.width) this.x = 0;
     }
@@ -24,14 +25,14 @@ var canvas_bg = document.getElementById('canvas-bg');
 canvas_bg.width = 910;
 canvas_bg.height = 422;
 
+// get context of the canvas
 var context = canvas_bg.getContext('2d');
 var bgImage = new LoadingImage();
-var bgObject = new BackgroundObject();
+var bgObject = new BackgroundObject(bgImage);
 
 
 // start the loop
 function start() {
     bgObject.draw();
 }
-
 setInterval(start, 10);
