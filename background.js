76d3469792acdf6f24b65487d129990c12795d31
Dong Liang <dongliang3571@gmail.com>
2017-01-09 22:33:35 -0500

    able to jump with space key

--- background.js
+++ background.js
@@ -19,33 +19,42 @@ function BackgroundObject(image) {
         context_bg.drawImage(this.image.backgroundImage, canvas_bg.width+this.x, this.y, canvas_bg.width, canvas_bg.height);
         this.x -= this.speed;
         if (this.x <= -canvas_bg.width) this.x = 0;
-    }
+    };
 }
 
-function BirdObject(image, birdSize) {
+function BirdObject(image, birdSize, canvas_fg) {
+    this.alive = true;
+    this.start = false;
     this.acceleration = 9.8;    // Acceleration for earth is 9.8 m/t^2
     this.vi = -5;   // initial speed
     this.vf = 0;    // final speed
-    this.x = 30;
-    this.y = canvas_fg.height/2;
-    this.push_speed = -5;
-    this.draw = function() {
-        context_fg.clearRect(0, 0, canvas_fg.width, canvas_fg.height);
-        context_fg.drawImage(image.birdImage, this.x, this.y, birdSize, birdSize);
-        context_fg.drawImage(image.birdImage, canvas_fg.width+this.x, this.y, birdSize, birdSize);
-        this.x -= this.speed;
-        if (this.x <= -canvas_fg.width) this.x = 0;
-    }
+    this.x = 30;    // inital x position
+    this.y = canvas_fg.height/2;    // initial y position
+    this.push_speed = -7;   // speed when click is triggered
     this.defaultDraw = function() {
+        if (!this.alive) {
+            console.log("game over");
+            // TODO: dispatch eventListener
+        }
         context_fg.clearRect(0, 0, canvas_fg.width, canvas_fg.height);
         context_fg.drawImage(image.birdImage, this.x, this.y, birdSize, birdSize);
-        this.vf = this.acceleration*1.0/60 + this.vi;
+        this.vf = this.acceleration*1.0/60 + this.vi;   // forumula: vf = at+vi
         this.vi = this.vf;
         this.y += this.vf;
-        if (this.y >= canvas_fg.height/2) {
+        if (this.y >= canvas_fg.height/2 && !this.start) {
             this.vi = this.push_speed;
+        } else {
+            if (KEY_STATUS.space) {
+                this.vi = this.push_speed;
+                this.start = true;
+                KEY_STATUS.space = false;
+            }
+
+            if (this.y >= canvas_fg.height-birdSize) {
+                this.alive = false;
+            }
         }
-    }
+    };
 }
 
 
@@ -62,16 +71,36 @@ var context_bg = canvas_bg.getContext('2d');
 var context_fg = canvas_fg.getContext('2d');
 var images = new LoadingImages();
 var bgObject = new BackgroundObject(images);
-var birdObject = new BirdObject(images, 80);
+var birdObject = new BirdObject(images, 80, canvas_fg);
 
+// initialization the game
+function addListeners() {
+    document.addEventListener("keydown", function(e) {
+        var keyCode = (e.keyCode) ? e.keyCode : e.charCode;
+        console.log(keyCode);
+        if (KEY_CODES[keyCode]) {
+            e.preventDefault();
+            KEY_STATUS[KEY_CODES[keyCode]] = true;
+        }
+    });
+}
+
+var KEY_CODES = {
+  32: 'space'
+}
+
+var KEY_STATUS = {};
+for (code in KEY_CODES) {
+    KEY_STATUS[KEY_CODES[code]] = false;
+}
 
 // start the loop
 function start() {
     bgObject.draw();
     birdObject.defaultDraw();
+    // birdObject.draw();
     window.requestAnimationFrame(start);
 }
 
+addListeners();
 start();
-// var fps = 60;
-// setInterval(start, 1000/fps);
